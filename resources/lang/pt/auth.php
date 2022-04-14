ee024c777d0d67072655b0cd49a95faebd491f9e
Pedro Barradas <pebarradas@gmail.com>
2022-04-14 15:25:29 +0100

    move

--- resources/lang/pt/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'failed'   => 'As suas credenciais não correspondem aos registos.',
-    'password' => 'A senha está incorreta.',
-    'throttle' => 'Várias tentativas de login. Tente novamente em : segundos.',
-];
